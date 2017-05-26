48f80172555be4ffeb0c6221ecfc26d417e133fc
Nicolas Röchter <rr@tobit.com>
2017-05-26 14:48:06 +0200

    Init cWalze-construcor / Method "BerechneCodierung"
    BerechneCodierung wandelt den Code-String der jeweiligen Walze in int-Werte um und speichert sie in dem int-Array

--- Enigma/cWalze.cs
+++ Enigma/cWalze.cs
@@ -6,6 +6,40 @@ using System.Threading.Tasks;
 
 namespace Enigma {
     class cWalze {
+        private int[] fVerschluesselungstabelle;
 
+        public cWalze(int pWalzennummer) {
+            switch (pWalzennummer) {
+                case 1:
+                    fVerschluesselungstabelle = BerechneCodierung("EKMFLGDQVZNTOWYHXUSPAIBRCJ");
+                    break;
+                case 2:
+                    fVerschluesselungstabelle = BerechneCodierung("AJDKSIRUXBLHWTMCQGZNPYFVOE");
+                    break;
+                case 3:
+                    fVerschluesselungstabelle = BerechneCodierung("BDFHJLCPRTXVZNYEIWGAKMUSQO");
+                    break;                 
+                case 4:
+                    fVerschluesselungstabelle = BerechneCodierung("ESOVPZJAYQUIRHXLNFTGKDCMWB");
+                    break;
+                case 5:
+                    fVerschluesselungstabelle = BerechneCodierung("VZBRGITYUPSDNHLXAWMJQOFECK");
+                    break;
+            }
+        }
+
+        private int[] BerechneCodierung(string pWalzenCode) {
+            int[] codierung = new int[26];
+            var counter = 0;
+
+            foreach(char i in pWalzenCode) {
+                var currentValue = (counter + 1) + (int) char.GetNumericValue(i);
+                if (currentValue > 90)
+                    currentValue -= 26;
+                codierung[counter] = currentValue;
+            }
+
+            return codierung;
+        }
     }
 }
